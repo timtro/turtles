a0d9d2ba68bae70cc7dd921bd142e568d9eea7ec
timtro <timtro@gmail.com>
2017-10-14 20:02:02 -0400

    Got rid of unneded commented code

--- oo-turtle/oo-turtle.cpp
+++ oo-turtle/oo-turtle.cpp
@@ -2,24 +2,7 @@
 #include <iostream>
 #include <sstream>
 
-// #include <SFML/Graphics.hpp>
-
 int main() {
-  // sf::RenderWindow window(sf::VideoMode(600, 600), "Turtle.");
-  // sf::CircleShape shape(100.f);
-  // shape.setFillColor(sf::Color::Red);
-
-  // while (window.isOpen()) {
-  //   sf::Event event;
-  //   while (window.pollEvent(event)) {
-  //     if (event.type == sf::Event::Closed)
-  //       window.close();
-  //   }
-
-  //   window.clear();
-  //   window.draw(shape);
-  //   window.display();
-  // }
 
   std::ostringstream oss;
   OOTurtle turtle(oss);
