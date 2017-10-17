747a9353840742810e6daf67d30a57f51485a693
timtro <timtro@gmail.com>
2017-10-17 09:29:53 -0400

    Refactoring/renaming for consistency and simplicity.

--- state-monad-turtle/Pose.cpp
+++ state-monad-turtle/Pose.cpp
@@ -10,12 +10,12 @@ auto mget() {
   return [](auto s) { return std::make_pair(s, s); };
 }
 
-M<double> move(double r, const Pose &p0) {
+StateWith<double> move(double r, const Pose &p0) {
   const auto dx = r * cos(p0.th);
   const auto dy = r * sin(p0.th);
   return {r, {p0.x + dx, p0.y + dy, p0.th}};
 }
 
-M<degree_t> turn(degree_t dth, const Pose &p0) {
+StateWith<degree_t> turn(degree_t dth, const Pose &p0) {
   return {dth, {p0.x, p0.y, degree_t{std::fmod((p0.th + dth)(), 360)}}};
 }
--- state-monad-turtle/Pose.hpp
+++ state-monad-turtle/Pose.hpp
@@ -9,26 +9,24 @@
 #define ENABLE_ANGLE_UNITS
 using units::angle::degree_t;
 
-struct Pose;
-
-template <typename T>
-using M = std::pair<T, Pose>;
-
-M<double> move(double, const Pose &);
-M<degree_t> turn(degree_t, const Pose &);
 
 struct Pose {
   const double x{0}, y{0};
   const degree_t th{0};
-
-  // These enable the dot().continuation().style().of().pipes().
-  M<double> move(double r) { return ::move(r, *this); }
-  M<degree_t> turn(degree_t dth) { return ::turn(dth, *this); }
 };
 
-template <class X>
-auto mreturn(const X &x) {
-  return [=](auto s) { return std::make_pair(x, s); };
+template <typename T>
+using StateWith = std::pair<T, Pose>;
+
+StateWith<double> move(double, const Pose &);
+StateWith<degree_t> turn(degree_t, const Pose &);
+
+// Functions supporting the State-Monad for the Turtle application.
+//
+// mreturn : 
+template <class A>
+auto mreturn(const A &a) {
+  return [=](auto stateData) { return std::make_pair(a, stateData); };
 }
 
 // Generally,
@@ -37,7 +35,9 @@ auto mreturn(const X &x) {
 //   M<T> = State<Pose, T> = = Pose → (T, Pose)
 // so,
 //   mbind : State<Pose, A> → (A → State<Pose, B>) → State<Pose, B>.
-// The bind operation gives us function composition.
+// The bind operation gives us function composition. NOTE: I think the
+// convention of calling the function type Pose → (T, Pose) a `State` is
+// misleading. It should be called a state-function or state-processor.
 template <class State, class F>
 auto mbind(State m, F f) {
   return [=](auto stateData) {
@@ -50,12 +50,14 @@ auto mbind(State m, F f) {
 
 // Mimick Haskell's `do` notation by automatically binding arguments together,
 // each of which should be an element in the monad:
-template <class X, class Y>
-auto mdo(X &&x, Y &&y) {
-  return mbind(x, [=](auto) { return y; });
+template <class A, class B>
+auto mdo(A &&a, B &&b) {
+  //          Throw away return values
+  //                    V                  
+  return mbind(a, [=](auto) { return b; });
 }
 
-template <class X, class... Xs>
-auto mdo(X &&x, Xs &&... xs) {
-  return mdo(x, mdo(xs...));
+template <class A, class... As>
+auto mdo(A &&a, As &&... as) {
+  return mdo(a, mdo(as...));
 }
